04b32c2b5c018858632d42ea9dbaa5b90cb5f63b
Orest95 <orest.if.ua@gmail.com>
2016-05-17 19:47:26 +0300

    all patterns

--- project/src/ChainOfResponsibilityApp.java
+++ project/src/ChainOfResponsibilityApp.java
@@ -2,4 +2,50 @@
  * Created by Orest on 17.05.2016.
  */
 public class ChainOfResponsibilityApp {
+    public static void main(String[] args) {
+        Telekanal near = new NearTransporter(10);
+        Telekanal full = new FullTransporter(60);
+        near.setNext(full);
+
+        near.loadCargo(8);
+        full.loadCargo(40);
+        near.loadCargo(40);
+    }
 }
+
+ abstract class Telekanal {
+    int time;
+     Telekanal next;
+
+    public void setNext(Telekanal next) {
+        this.next = next;
+    }
+
+    public Telekanal(int time) {
+        this.time = time;
+    }
+
+    public void loadCargo(int weight) {
+        if(weight<=time){
+            System.out.println("Реклама коротка, показ на телеканалі 24");
+        } else {
+            System.out.println("Реклама довга, перенесено на канал 1+1");
+            if(next!=null){
+                next.loadCargo(weight);
+            }
+        }
+    }
+}
+
+ class FullTransporter extends Telekanal {
+    public FullTransporter(int time) {
+        super(time);
+    }
+}
+
+class NearTransporter extends Telekanal{
+    public NearTransporter(int time) {
+        super(time);
+    }
+}
+
--- project/src/InterpreterApp.java
+++ project/src/InterpreterApp.java
@@ -1,8 +1,60 @@
 /**
  * Created by Orest on 17.05.2016.
  */
-public class InterpreterApp {
+ class InterpreterApp {
     public static void main(String[] args) {
 
+        System.out.println("Вартість реклами:");
+
+
+        String str = "9";
+        Context context = new Context(str);
+
+        Expression price = new TerminalExpression();
+        price.interpret(context);
+    }
+}
+
+ abstract class Expression {
+    public abstract void interpret(Context context);
+}
+
+
+ class Context {
+    private String input;
+    private String output;
+
+    public Context(String input) {
+        this.input = input;
+        this.output = "";
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public void setOutput(String output) {
+        this.output = output;
+    }
+}
+
+ class TerminalExpression extends Expression {
+    @Override
+    public void interpret(Context context) {
+        double price;
+        String input = context.getInput();
+        price = Integer.valueOf(input);
+        price = price * 3000;
+
+        context.setOutput(Double.toString(price) + " грн");
+        System.out.println(context.getOutput());
     }
 }
